975b40920cccc2f991a6de316bf4857e9ae55a0a
zhongpei <1000@intry.cn>
2022-11-16 18:22:41 +0800

    BaseEntity_TenantId

---
+++ host/IczpNet.Organization.HttpApi.Host/Migrations/20221116102025_BaseEntity_TenantId.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IczpNet.Organization.Migrations
+{
+    public partial class BaseEntity_TenantId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "TenantId",
+                table: "Organization_PostGradePostLevel",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TenantId",
+                table: "Organization_EmployeePosition",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "Organization_PostGradePostLevel");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "Organization_EmployeePosition");
+        }
+    }
+}

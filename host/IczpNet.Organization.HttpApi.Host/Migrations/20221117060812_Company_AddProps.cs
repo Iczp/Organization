6087a99d03f7e20021c0aced49ca0b764c3cae6e
zhongpei <1000@intry.cn>
2022-11-17 14:11:43 +0800

    Company_AddProps

---
+++ host/IczpNet.Organization.HttpApi.Host/Migrations/20221117060812_Company_AddProps.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IczpNet.Organization.Migrations
+{
+    public partial class Company_AddProps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Organization_Company",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BusinessLicenseEndDate",
+                table: "Organization_Company",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BusinessLicenseStartDate",
+                table: "Organization_Company",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreditCode",
+                table: "Organization_Company",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FoundDate",
+                table: "Organization_Company",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InvoiceTitle",
+                table: "Organization_Company",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LealPerson",
+                table: "Organization_Company",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Organization_Company",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "RegisteredCapital",
+                table: "Organization_Company",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegistrationDate",
+                table: "Organization_Company",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TaxNo",
+                table: "Organization_Company",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Organization_Company");
+
+            migrationBuilder.DropColumn(
+                name: "BusinessLicenseEndDate",
+                table: "Organization_Company");
+
+            migrationBuilder.DropColumn(
+                name: "BusinessLicenseStartDate",
+                table: "Organization_Company");
+
+            migrationBuilder.DropColumn(
+                name: "CreditCode",
+                table: "Organization_Company");
+
+            migrationBuilder.DropColumn(
+                name: "FoundDate",
+                table: "Organization_Company");
+
+            migrationBuilder.DropColumn(
+                name: "InvoiceTitle",
+                table: "Organization_Company");
+
+            migrationBuilder.DropColumn(
+                name: "LealPerson",
+                table: "Organization_Company");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Organization_Company");
+
+            migrationBuilder.DropColumn(
+                name: "RegisteredCapital",
+                table: "Organization_Company");
+
+            migrationBuilder.DropColumn(
+                name: "RegistrationDate",
+                table: "Organization_Company");
+
+            migrationBuilder.DropColumn(
+                name: "TaxNo",
+                table: "Organization_Company");
+        }
+    }
+}
--- src/IczpNet.Organization.Domain/Companys/Company.cs
+++ src/IczpNet.Organization.Domain/Companys/Company.cs
@@ -1,12 +1,41 @@
 ﻿using IczpNet.Organization.Bases;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Volo.Abp.MultiTenancy;
 
 namespace IczpNet.Organization.Companys
 {
     public class Company : BaseTreeEntity<Company>, IMultiTenant
     {
-        //public virtual string Description { get; set; }
 
+        [MaxLength(40)]
+        public virtual string CreditCode { get; set; }
 
+        [MaxLength(40)]
+        public virtual string LealPerson { get; set; }
+
+        public virtual DateTime? RegistrationDate { get; set; }
+
+        public virtual DateTime? FoundDate { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public virtual decimal RegisteredCapital { get; set; }
+
+        public virtual DateTime? BusinessLicenseStartDate { get; set; }
+
+        public virtual DateTime? BusinessLicenseEndDate { get; set; }
+
+        [MaxLength(40)]
+        public virtual string InvoiceTitle { get; set; }
+
+        [MaxLength(40)]
+        public virtual string TaxNo { get; set; }
+
+        [MaxLength(20)]
+        public virtual string Phone { get; set; }
+
+        [MaxLength(200)]
+        public virtual string Address { get; set; }
     }
 }

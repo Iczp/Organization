72da00ff337a746ddb6fbdaa42ab8da40a5d00ea
zhongpei <1000@intry.cn>
2022-11-18 11:29:51 +0800

    Entity_GlobalDropProp_IsStatic

---
+++ host/IczpNet.Organization.HttpApi.Host/Migrations/20221118032908_Entity_GlobalDropProp_IsStatic..cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IczpNet.Organization.Migrations
+{
+    public partial class Entity_GlobalDropProp_IsStatic : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsStatic",
+                table: "Organization_PostGradePostLevel");
+
+            migrationBuilder.DropColumn(
+                name: "IsStatic",
+                table: "Organization_PositionFunctional");
+
+            migrationBuilder.DropColumn(
+                name: "IsStatic",
+                table: "Organization_EmployeePosition");
+
+            migrationBuilder.DropColumn(
+                name: "IsStatic",
+                table: "Organization_DepartmentPosition");
+
+            migrationBuilder.DropColumn(
+                name: "IsStatic",
+                table: "Organization_DepartmentFunctional");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsStatic",
+                table: "Organization_PostGradePostLevel",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsStatic",
+                table: "Organization_PositionFunctional",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsStatic",
+                table: "Organization_EmployeePosition",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsStatic",
+                table: "Organization_DepartmentPosition",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsStatic",
+                table: "Organization_DepartmentFunctional",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}

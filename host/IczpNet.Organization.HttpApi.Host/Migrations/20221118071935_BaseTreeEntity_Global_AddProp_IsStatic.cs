64c1f88840a4a04995fab6275dea5c15a88739cb
zhongpei <1000@intry.cn>
2022-11-18 15:20:11 +0800

    BaseTreeEntity_Global_AddProp_IsStatic

---
+++ host/IczpNet.Organization.HttpApi.Host/Migrations/20221118071935_BaseTreeEntity_Global_AddProp_IsStatic.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IczpNet.Organization.Migrations
+{
+    public partial class BaseTreeEntity_Global_AddProp_IsStatic : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsStatic",
+                table: "Organization_Position",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsStatic",
+                table: "Organization_Functional",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsStatic",
+                table: "Organization_Department",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsStatic",
+                table: "Organization_Company",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsStatic",
+                table: "Organization_Position");
+
+            migrationBuilder.DropColumn(
+                name: "IsStatic",
+                table: "Organization_Functional");
+
+            migrationBuilder.DropColumn(
+                name: "IsStatic",
+                table: "Organization_Department");
+
+            migrationBuilder.DropColumn(
+                name: "IsStatic",
+                table: "Organization_Company");
+        }
+    }
+}

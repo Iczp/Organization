9ac9a15652ece523efed893cc6ce16134249cc35
zhongpei <1000@intry.cn>
2022-11-18 09:37:04 +0800

    CrudCommonAppService

--- src/IczpNet.Organization.Application/Bases/CrudOrganizationAppService.cs
+++ src/IczpNet.Organization.Application/Bases/CrudOrganizationAppService.cs
@@ -1,14 +1,10 @@
-﻿using IczpNet.AbpCommons.DataFilters;
+﻿using IczpNet.AbpCommons;
 using IczpNet.Organization.Localization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
-using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Entities;
 using Volo.Abp.Domain.Repositories;
-using Volo.Abp.MultiTenancy;
 
 namespace IczpNet.Organization.Bases;
 
@@ -21,7 +17,7 @@ public abstract class CrudOrganizationAppService<
     TCreateInput,
     TUpdateInput>
     :
-    CrudAppService<
+    CrudCommonAppService<
         TEntity,
         TGetOutputDto,
         TGetListOutputDto,
@@ -54,80 +50,15 @@ public abstract class CrudOrganizationAppService<
         return base.GetAsync(id);
     }
 
-    [HttpGet]
-    public virtual async Task<List<TGetOutputDto>> GetManyAsync(List<TKey> idList)
-    {
-        var list = new List<TGetOutputDto>();
-
-        foreach (var id in idList)
-        {
-            list.Add(await base.GetAsync(id));
-        }
-        return list;
-    }
-
-    [HttpGet]
-    public override Task<PagedResultDto<TGetListOutputDto>> GetListAsync(TGetListInput input)
-    {
-        return base.GetListAsync(input);
-    }
-
-    [HttpPost]
-    public override async Task<TGetOutputDto> CreateAsync(TCreateInput input)
-    {
-        await CheckCreatePolicyAsync();
-
-        var entity = await MapToEntityAsync(input);
-
-        await SetCreateEntityAsync(entity, input);
-
-        TryToSetTenantId(entity);
-
-        await Repository.InsertAsync(entity, autoSave: true);
-
-        return await MapToGetOutputDtoAsync(entity);
-    }
-
-    protected virtual Task SetCreateEntityAsync(TEntity entity, TCreateInput input)
+    protected override Task SetCreateEntityAsync(TEntity entity, TCreateInput input)
     {
         return Task.CompletedTask;
     }
 
 
-
-    [HttpPost]
-    public override async Task<TGetOutputDto> UpdateAsync(TKey id, TUpdateInput input)
-    {
-        await CheckUpdatePolicyAsync();
-
-        var entity = await GetEntityByIdAsync(id);
-        //TODO: Check if input has id different than given id and normalize if it's default value, throw ex otherwise
-        await MapToEntityAsync(input, entity);
-
-        await SetUpdateEntityAsync(entity, input);
-
-        await Repository.UpdateAsync(entity, autoSave: true);
-
-        return await MapToGetOutputDtoAsync(entity);
-    }
-
-    protected virtual Task SetUpdateEntityAsync(TEntity entity, TUpdateInput input)
+    protected override Task SetUpdateEntityAsync(TEntity entity, TUpdateInput input)
     {
         return Task.CompletedTask;
     }
 
-    [HttpPost]
-    public override Task DeleteAsync(TKey id)
-    {
-        return base.DeleteAsync(id);
-    }
-
-    [HttpPost]
-    public virtual async Task DeleteManyAsync(List<TKey> idList)
-    {
-        foreach (var id in idList)
-        {
-            await base.DeleteAsync(id);
-        }
-    }
 }
